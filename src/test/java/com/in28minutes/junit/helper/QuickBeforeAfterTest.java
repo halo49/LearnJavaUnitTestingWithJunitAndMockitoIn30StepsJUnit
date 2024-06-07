ef6a211fa0d293b5cd11f4e754476c4fa4b39f90
halo49 <83737814+halo49@users.noreply.github.com>
2024-06-07 15:18:31 -0600

    Comparing Arrays

--- src/test/java/com/in28minutes/junit/helper/QuickBeforeAfterTest.java
+++ src/test/java/com/in28minutes/junit/helper/QuickBeforeAfterTest.java
@@ -1,11 +1,20 @@
 package com.in28minutes.junit.helper;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class QuickBeforeAfterTest {
 
+	// Se ejecuta una sola vez antes de todos los Test y debe ser static
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before Class");
+	}
+	
+	// Antes de cada Test
 	@Before
 	public void setup() {
 		System.out.println("Before Test");
@@ -21,9 +30,17 @@ public class QuickBeforeAfterTest {
 		System.out.println("test 2 executed");
 	}
 	
+	// Despues de cada Test
 	@After
 	public void teardown() {
 		System.out.println("After test");
 	}
+
+	// Se ejecuta una sola vez despues de todos los Test y debe ser static
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After Class");
+	}
+
 	
 }
