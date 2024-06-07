a5a8e9590a9a5aef07d7900aa01fba3a3570d9c6
halo49 <83737814+halo49@users.noreply.github.com>
2024-06-06 23:44:11 -0600

    Successful Junit with assertEquals

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -6,9 +6,14 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	// El test pasará si no hay ningún fail
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		StringHelper helper = new StringHelper();
+		// expected, actual
+		// AACD => CDD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 	}
 
 }
