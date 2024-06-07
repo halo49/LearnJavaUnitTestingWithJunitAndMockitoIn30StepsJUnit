b842d36e787b6c7dc963914b9775bd2d6f97cfe9
halo49 <83737814+halo49@users.noreply.github.com>
2024-06-07 00:06:28 -0600

    Refactoring the Junit test

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -6,14 +6,39 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	StringHelper helper = new StringHelper();
+	
 	// El test pasará si no hay ningún fail
+	// No es bueno tener mas de una condicion en el test
+	// Cada metodo de test debe regresar void y debe ser publico
+	// El nombre del metodo del test comenzara con test y el nombre
+	// del metodo que se quiera probar y luego _ y la condicion que se esta probando
 	@Test
-	public void test() {
-		StringHelper helper = new StringHelper();
+	public void testTruncateAInFirst2Positions_Aintfirst2Positions() {
 		// expected, actual
 		// AACD => CDD, ACD => CD, CDEF => CDEF, CDAA => CDAA
 		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 	}
 
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirstPosition() {
+		// expected, actual
+		// AACD => CDD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_NoA() {
+		// expected, actual
+		// AACD => CDD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_NoChange() {
+		// expected, actual
+		// AACD => CDD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+	}
+	
 }
