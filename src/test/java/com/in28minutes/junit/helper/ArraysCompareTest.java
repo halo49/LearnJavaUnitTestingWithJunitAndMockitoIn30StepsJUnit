0c761c3a941677f35fc4a7982effbf8335fdda97
halo49 <83737814+halo49@users.noreply.github.com>
2024-06-07 15:24:04 -0600

    Handling Exceptions

--- src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -8,14 +8,18 @@ import org.junit.Test;
 
 public class ArraysCompareTest {
 
-	// Arrays.sort
-	
 	@Test
 	public void testArraySort_RandomArray() {
 		int[] numbers = {12, 3, 4, 1};
 		int[] expected = {1, 3, 4, 12};
 		Arrays.sort(numbers);
 		assertArrayEquals(expected, numbers);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testArraySort_NullArray() {
+		int[] numbers = null;
+		Arrays.sort(numbers);
 	}
 	
 }
